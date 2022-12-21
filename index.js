9a2b23e34b6009edaf52d57e0559245a9858f21f
rubensato <rubensato@gmail.com>
2022-12-21 17:30:49 -0300

    configuracion para usar CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 /* EXPRESS*/
 const app = express();
@@ -8,12 +9,17 @@ app.use(express.json());
 /* MONGOOSE - CONEXION A BASE DE DATOS*/
 require('./config/db');
 
+/* CORS*/
+app.use(cors());
 
+/* ROUTES */
+
+/* EXPRESS SERVER RUNNING OK*/
 app.get('/', (req, res) => {
     res.send('Servidor ok');
   });
 
-/* SERVER RUNNING AND LISTENING PORT*/
+/* EXPRESS SERVER RUNNING AND LISTENING PORT*/
 app.listen(process.env.PORT || 3000, () => {
     console.log(
       `MCGA_2022_Backend_API ~ Online - Running on PORT: ${process.env.PORT || 3000}`
