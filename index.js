2bef71b9d3625a3b4be59bda1888c39e20005e8c
rubensato <rubensato@gmail.com>
2022-12-21 17:07:10 -0300

    configuracion inicial del servidor express

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+/* EXPRESS*/
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Servidor ok');
+  });
+
+/* SERVER RUNNING AND LISTENING PORT*/
+app.listen(process.env.PORT || 3000, () => {
+    console.log(
+      `MCGA_2022_Backend_API ~ Online - Running on PORT: ${process.env.PORT || 3000}`
+    );
+  });
+  
